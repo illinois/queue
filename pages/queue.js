692da94d031846d357f7dc1aad792120f565a9be
Nathan Walters <nwalters512@gmail.com>
2018-02-20 02:40:48 -0600

    Improve layout on medium-sized devices

--- pages/queue.js
+++ pages/queue.js
@@ -41,13 +41,13 @@ class Queue extends React.Component {
       <Layout>
         <Container fluid>
           <Row>
-            <Col xs={{ size: 12 }} md={{ size: 3 }} className="mb-3 mb-md-0">
+            <Col xs={{ size: 12 }} md={{ size: 4 }}  lg={{ size: 3 }} className="mb-3 mb-md-0">
               <ShowForCourseStaff queueId={this.props.queueId}>
                 <QuestionNotificationsToggle />
               </ShowForCourseStaff>
               <StaffSidebar queueId={this.props.queueId} />
             </Col>
-            <Col xs={{ size: 12 }} md={{ size: 9 }} className="mb-3">
+            <Col xs={{ size: 12 }} md={{ size: 8 }} lg={{ size: 9 }} className="mb-3">
               <QuestionPanel queueId={this.props.queueId} />
               <QuestionListContainer queueId={this.props.queueId} />
             </Col>
