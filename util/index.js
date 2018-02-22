ef9c9d81b16c3183051cf3f347457b24bcda819c
Nathan Walters <nwalters512@gmail.com>
2018-02-22 16:46:23 -0600

    Add support for parrots :fastparrot:

--- util/index.js
+++ util/index.js
@@ -3,3 +3,5 @@
 module.exports.baseUrl = (typeof window !== 'undefined' && window.BASE_URL)
                       || (typeof process !== 'undefined' && process.env.BASE_URL)
                       || ''
+
+module.exports.withBaseUrl = url => `${module.exports.baseUrl}${url}`
