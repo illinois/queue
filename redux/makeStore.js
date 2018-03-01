131c322c6c9658348ec9091fcfa58cb66d0cf7ae
Nathan Walters <nwalters512@gmail.com>
2018-02-28 19:35:30 -0600

    Fix linter error

--- redux/makeStore.js
+++ redux/makeStore.js
@@ -1,10 +1,10 @@
 /* eslint-env browser */
+/* eslint-disable no-underscore-dangle */
 import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 import reducer from '../reducers'
 import questionNotificationMiddleware from './questionNotificationMiddleware'
 
-// eslint-disable-next-line no-underscore-dangle
 const composeEnhancers =
   (typeof window !== 'undefined' &&
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
