065ac2517bfee310fb3100b4857320c8426d7f3d
Nathan Walters <nwalters512@gmail.com>
2018-03-09 16:38:46 -0600

    Add page footer with link to GitHub repo

--- components/Layout.js
+++ components/Layout.js
@@ -1,15 +1,26 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import PropTypes from 'prop-types'
 
 import Header from './Header'
+import Footer from './Footer'
 
 const Layout = props => (
-  <div>
+  <Fragment>
     <Header />
     {props.children}
     {/* This provides a bit of padding at the bottom of the page */}
-    <div className="mt-3" />
-  </div>
+    <Footer />
+    <style global jsx>{`
+      html {
+        height: 100%;
+      }
+      body {
+        min-height: 100%;
+        position: relative;
+        padding-bottom: 5rem;
+      }
+    `}</style>
+  </Fragment>
 )
 
 Layout.propTypes = {
