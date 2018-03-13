01eae6a94a38c55abc1a69c8afb669536fdc9e5b
Nathan Walters <nwalters512@gmail.com>
2018-03-13 18:05:59 -0500

    Add a few more migrations

--- migrations/20180312225409-create-course-staff.js
+++ migrations/20180312225409-create-course-staff.js
@@ -1,23 +1,40 @@
-'use strict';
-
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
-    */
+    queryInterface.createTable('courseStaff', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      courseId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'courses',
+          key: 'id',
+        },
+        allowNull: false,
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'users',
+          key: 'id',
+        },
+        allowNull: false,
+      }
+    })
   },
 
-  down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.dropTable('users');
-    */
+  down: (queryInterface, _Sequelize) => {
+    return queryInterface.dropTable('courseStaff')
   }
 };
--- migrations/20180312225413-create-questions.js
+++ migrations/20180312225413-create-questions.js
@@ -1,23 +1,66 @@
-'use strict';
-
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
+    queryInterface.createTable('courseStaff', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
 
-      Example:
-      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
-    */
+      name: Sequelize.TEXT,
+      location: Sequelize.TEXT,
+      topic: Sequelize.TEXT,
+      beingAnswered: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
+      },
+      answerStartTime: Sequelize.DATE,
+      answerFinishTime: Sequelize.DATE,
+      enqueueTime: Sequelize.DATE,
+      dequeueTime: Sequelize.DATE,
+      comments: Sequelize.TEXT,
+      preparedness: Sequelize.TEXT,
+
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      deletedAt: {
+        type: Sequelize.DATE
+      },
+      queueId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'queues',
+          key: 'id',
+        },
+        allowNull: false,
+      },
+      askedById: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'users',
+          key: 'id',
+        },
+        allowNull: false,
+      },
+      answeredById: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'users',
+          key: 'id',
+        },
+        allowNull: false,
+      }
+    })
   },
 
-  down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.dropTable('users');
-    */
+  down: (queryInterface, _Sequelize) => {
+    return queryInterface.dropTable('questions')
   }
 };
