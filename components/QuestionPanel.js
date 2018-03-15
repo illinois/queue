d3acf31376e7d084fe5cb4ed70013986d36837f4
ghelsel2 <ghelsel2@illinois.edu>
2018-03-15 11:24:08 -0500

    hide question panel for active staff

--- components/QuestionPanel.js
+++ components/QuestionPanel.js
@@ -2,12 +2,18 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 
-import { getUserActiveQuestionIdForQueue } from '../selectors'
+import { getUserActiveQuestionIdForQueue,
+        isUserActiveStaffForQueue
+        } from '../selectors'
+
 
 import ActiveQuestionPanel from './ActiveQuestionPanel'
 import NewQuestionContainer from '../containers/NewQuestionContainer'
 
-const QuestionPanel = ({ queueId, user, userActiveQuestionId }) => {
+const QuestionPanel = ({ queueId, user, userActiveQuestionId, isUserActiveStaff }) => {
+  if (isUserActiveStaff) {
+      return null
+  }
   if (userActiveQuestionId !== -1) {
     return (
       <ActiveQuestionPanel
@@ -25,11 +31,13 @@ QuestionPanel.propTypes = {
     name: PropTypes.string,
   }).isRequired,
   userActiveQuestionId: PropTypes.number.isRequired,
+  isUserActiveStaff: PropTypes.bool.isRequired,
 }
 
 const mapStateToProps = (state, ownProps) => ({
   user: state.user.user,
   userActiveQuestionId: getUserActiveQuestionIdForQueue(state, ownProps),
+  isUserActiveStaff: isUserActiveStaffForQueue(state, ownProps),
 })
 
 export default connect(mapStateToProps, null)(QuestionPanel)
