2b5aae30fe2631be0c3f65206277a50682c95c47
Nathan Walters <nwalters512@gmail.com>
2018-03-16 09:59:08 -0500

    Show success/error message when updating preferred name

--- components/UserProfileSettings.js
+++ components/UserProfileSettings.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Form, Input, FormText, Button } from 'reactstrap'
+import { Form, Input, FormText, Button, Alert, Collapse } from 'reactstrap'
+
+import { UPDATE_USER_PREFERRED_NAME } from '../constants/ActionTypes'
 
 class UserProfileSettings extends React.Component {
   constructor(props) {
@@ -8,9 +10,18 @@ class UserProfileSettings extends React.Component {
 
     this.state = {
       name: (props.user && props.user.preferredName) || '',
+      preferredNameUpdateStatus: {
+        succeeded: null,
+        message: null,
+        isOpen: false,
+      },
     }
 
     this.handleInputChange = this.handleInputChange.bind(this)
+  }
+
+  componentWillUnmount() {
+    this.clearPreferredNameUpdateStatusTimeout()
   }
 
   handleInputChange(event) {
@@ -21,7 +32,42 @@ class UserProfileSettings extends React.Component {
 
   handleChangePreferredName(e) {
     if (e) e.preventDefault()
-    this.props.updateUserPreferredName(this.state.name)
+    this.props.updateUserPreferredName(this.state.name).then(action => {
+      if (action && action.type === UPDATE_USER_PREFERRED_NAME.SUCCESS) {
+        this.setState({
+          preferredNameUpdateStatus: {
+            message: 'Successfully updated preferred name!',
+            succeeded: true,
+            isOpen: true,
+          },
+        })
+      } else {
+        this.setState({
+          preferredNameUpdateStatus: {
+            message: 'Failed to update preferred name. Please try again later.',
+            succeeded: false,
+            isOpen: true,
+          },
+        })
+      }
+      this.clearPreferredNameUpdateStatusTimeout()
+      this.preferredNameUpdateStatusTimeout = setTimeout(() => {
+        this.setState({
+          preferredNameUpdateStatus: {
+            ...this.state.preferredNameUpdateStatus,
+            isOpen: false,
+          },
+        })
+        this.preferredNameUpdateStatusTimeout = null
+      }, 3000)
+    })
+  }
+
+  clearPreferredNameUpdateStatusTimeout() {
+    if (this.preferredNameUpdateStatusTimeout) {
+      clearTimeout(this.preferredNameUpdateStatusTimeout)
+      this.preferredNameUpdateStatusTimeout = null
+    }
   }
 
   render() {
@@ -61,6 +107,18 @@ class UserProfileSettings extends React.Component {
               Update
             </Button>
           </Form>
+          <Collapse isOpen={!!this.state.preferredNameUpdateStatus.isOpen}>
+            <Alert
+              color={
+                this.state.preferredNameUpdateStatus.succeeded
+                  ? 'success'
+                  : 'danger'
+              }
+              className="mt-3 small"
+            >
+              {this.state.preferredNameUpdateStatus.message}
+            </Alert>
+          </Collapse>
           <FormText color="muted">
             If you go by a different name than that in the school&apos;s records
             or have a preferred nickname, you can enter that here. This will be
