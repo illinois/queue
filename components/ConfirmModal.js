4f6f8c8c69a59afb98774a1aa7d3ec781a5bb5be
muakasan <muakasan@gmail.com>
2018-03-17 22:56:50 -0500

    Made ConfirmModal and generalized CDQModal

---
+++ components/ConfirmModal.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Modal, ModalHeader, ModalBody, ModalFooter, Button } from 'reactstrap'
+
+const ConfirmModal = props => (
+  <Modal isOpen={props.isOpen} toggle={props.toggle}>
+    <ModalHeader>Are you sure?</ModalHeader>
+    <ModalBody>
+      {props.descText}
+    </ModalBody>
+    <ModalFooter>
+      <Button color="danger" onClick={props.confirm}>
+        {props.confirmText}
+      </Button>
+      <Button color="secondary" onClick={props.toggle}>
+        Cancel
+      </Button>
+    </ModalFooter>
+  </Modal>
+)
+
+ConfirmModal.propTypes = {
+  isOpen: PropTypes.bool.isRequired,
+  toggle: PropTypes.func.isRequired,
+  confirm: PropTypes.func.isRequired,
+  descText: PropTypes.string.isRequired,
+  confirmText: PropTypes.string.isRequired
+}
+
+export default ConfirmModal
