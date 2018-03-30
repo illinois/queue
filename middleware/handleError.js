5087a014dc630fd183ab0c30d600b1d52ef87cd9
Nathan Walters <nwalters512@gmail.com>
2018-03-30 14:43:39 -0500

    Handle edge case with errors

--- middleware/handleError.js
+++ middleware/handleError.js
@@ -2,5 +2,9 @@ const logger = require('../util/logger')
 
 module.exports = (err, req, res, _next) => {
   logger.error(err.stack)
+  if (res.headersSent) {
+    req.socket.destroy()
+    return
+  }
   res.status(500).send()
 }
