858ff3704b93c23da6328b29a490dc4a14ff988c
Jordi Paris Ferrer <jparisferrer@gmail.com>
2018-04-13 14:42:34 -0500

    Custom 404 Page with header, button complete

--- pages/_error.js
+++ pages/_error.js
@@ -1,59 +1,25 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import HTTPStatus from 'http-status'
-import Head from './head'
+import { Button } from 'reactstrap'
+import withRedux from 'next-redux-wrapper'
 import Layout from '../components/Layout'
-const { baseUrl } = require('./util')
-
-export default class Error extends React.Component {
-  static getInitialProps ({ res, err }) {
-    const statusCode = res ? res.statusCode : (err ? err.statusCode : null)
-    return { statusCode }
-  }
-
-  static propTypes = {
-    statusCode: PropTypes.number
-  }
-
-  render () {
-    const { statusCode } = this.props
-    const title = statusCode === 404
-      ? 'This page could not be found'
-      : HTTPStatus[statusCode] || 'An unexpected error has occurred'
-
-    return <div style={styles.error}>
-      <Head>
-        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
-      </Head>
-      <Layout>
-          <div>
-            <style dangerouslySetInnerHTML={{ __html: 'body { margin: 0 }' }} />
-            {statusCode ? <h1 style={styles.h1}>{statusCode}</h1> : null}
-            <div style={styles.desc}>
-              <h2 style={styles.h2}>{title}.</h2>
-            </div>
-            <div>
-                <Link href={{baseUrl}}>
-                    <Button color="primary" tag="a" size="lg"> </Button>
-                </Link>
-
-            </div>
-          </div>
-      </Layout>
-  }
-}
+import { Link } from '../routes'
+import makeStore from '../redux/makeStore'
+import PageWithUser from '../components/PageWithUser'
 
 const styles = {
   error: {
     color: '#000',
     background: '#fff',
-    fontFamily: '-apple-system, BlinkMacSystemFont, Roboto, "Segoe UI", "Fira Sans", Avenir, "Helvetica Neue", "Lucida Grande", sans-serif',
+    fontFamily:
+      '-apple-system, BlinkMacSystemFont, Roboto, "Segoe UI", "Fira Sans", Avenir, "Helvetica Neue", "Lucida Grande", sans-serif',
     height: '100vh',
     textAlign: 'center',
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
   },
 
   desc: {
@@ -61,7 +27,7 @@ const styles = {
     textAlign: 'left',
     lineHeight: '49px',
     height: '49px',
-    verticalAlign: 'middle'
+    verticalAlign: 'middle',
   },
 
   h1: {
@@ -72,7 +38,7 @@ const styles = {
     padding: '10px 23px 10px 0',
     fontSize: '24px',
     fontWeight: 500,
-    verticalAlign: 'top'
+    verticalAlign: 'top',
   },
 
   h2: {
@@ -80,6 +46,60 @@ const styles = {
     fontWeight: 'normal',
     lineHeight: 'inherit',
     margin: 0,
-    padding: 0
+    padding: 0,
+  },
+
+  navHomeButton: {
+    marginTop: '24px',
+  },
+}
+
+export class Error extends React.Component {
+  static getInitialProps({ res, err }) {
+    let statusCode = null
+    if (res) {
+      ;({ statusCode } = res)
+    } else if (err) {
+      ;({ statusCode } = err)
+    }
+
+    return { statusCode }
+  }
+
+  render() {
+    const { statusCode } = this.props
+    const title =
+      statusCode === 404
+        ? 'This page could not be found'
+        : HTTPStatus[statusCode] || 'An unexpected error has occurred'
+
+    return (
+      <Layout>
+        <div style={styles.error}>
+          <div>
+            {statusCode ? <h1 style={styles.h1}>{statusCode}</h1> : null}
+            <div style={styles.desc}>
+              <h2 style={styles.h2}>{title}.</h2>
+            </div>
+            <div style={styles.navHomeButton}>
+              <Link passHref route="index">
+                <Button outline color="secondary" tag="a" size="lg" block>
+                  {' '}
+                  Home{' '}
+                </Button>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </Layout>
+    )
   }
 }
+Error.defaultProps = {
+  statusCode: 404,
+}
+Error.propTypes = {
+  statusCode: PropTypes.number,
+}
+
+export default withRedux(makeStore, null, null)(PageWithUser(Error))
