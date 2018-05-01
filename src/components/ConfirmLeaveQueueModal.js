21fb7177d3dee31f9f1ed724ca7339bad1f39c57
Josh Byster <joshb1050@gmail.com>
2018-05-01 00:48:08 -0500

    Made descText and confirmText not required in the confirm modals

--- src/components/ConfirmLeaveQueueModal.js
+++ src/components/ConfirmLeaveQueueModal.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import ConfirmModal from './ConfirmModal'
 
 const ConfirmLeaveQueueModal = props => (
@@ -11,6 +12,10 @@ const ConfirmLeaveQueueModal = props => (
   />
 )
 
-ConfirmLeaveQueueModal.propTypes = ConfirmModal.propTypes
+ConfirmLeaveQueueModal.propTypes = {
+  isOpen: PropTypes.bool.isRequired,
+  toggle: PropTypes.func.isRequired,
+  confirm: PropTypes.func.isRequired,
+}
 
 export default ConfirmLeaveQueueModal
