5331716a6eecdf1070baa5de73aeb6c169847479
Nathan Walters <nwalters512@gmail.com>
2018-05-11 23:48:14 -0500

    Reduce transition time to 200ms

--- src/components/AppContainer.js
+++ src/components/AppContainer.js
@@ -8,7 +8,7 @@ import Footer from './Footer'
 const AppContainer = props => (
   <Fragment>
     <Header />
-    <TransitionManager timeout={300}>{props.children}</TransitionManager>
+    <TransitionManager timeout={200}>{props.children}</TransitionManager>
     <Footer />
     <style global jsx>{`
       html {
