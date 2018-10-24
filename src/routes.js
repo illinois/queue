d3dff43d57b059567d13c27553d7b7f1329056d2
Nathan Walters <nwalters512@gmail.com>
2018-10-24 18:46:44 -0500

    Fix index route with empty base URL

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,16 @@
 const routes = require('next-routes')()
-const { withBaseUrl } = require('./util')
+const { baseUrl, withBaseUrl } = require('./util')
 
-routes
-  .add('index', withBaseUrl(''))
-  .add('queue', withBaseUrl('/queue/:id'))
+// We need to special-case the index route. When running with a non-empty
+// base URL, for instance /foo, we'll fail to resolve /foo if the index URL 
+// ends with a slash. However, when running locally with an empty base URL,
+// we need the root route to be "/" for it to be resolved properly.
+if (baseUrl === '') {
+  routes.add('index', '/')
+} else {
+  routes.add('index', withBaseUrl('/'))
+}
+routes.add('queue', withBaseUrl('/queue/:id'))
   .add('createCourse', withBaseUrl('/course/create'))
   .add('course', withBaseUrl('/course/:id'))
   .add('courseStaff', withBaseUrl('/course/:id/staff'))
