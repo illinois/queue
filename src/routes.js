adae012bfca420f9ea6433f0697618725df6368a
Nathan Walters <nwalters512@gmail.com>
2018-10-24 18:53:24 -0500

    Run prettier

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,7 @@ const routes = require('next-routes')()
 const { baseUrl, withBaseUrl } = require('./util')
 
 // We need to special-case the index route. When running with a non-empty
-// base URL, for instance /foo, we'll fail to resolve /foo if the index URL 
+// base URL, for instance /foo, we'll fail to resolve /foo if the index URL
 // ends with a slash. However, when running locally with an empty base URL,
 // we need the root route to be "/" for it to be resolved properly.
 if (baseUrl === '') {
@@ -10,7 +10,8 @@ if (baseUrl === '') {
 } else {
   routes.add('index', withBaseUrl('/'))
 }
-routes.add('queue', withBaseUrl('/queue/:id'))
+routes
+  .add('queue', withBaseUrl('/queue/:id'))
   .add('createCourse', withBaseUrl('/course/create'))
   .add('course', withBaseUrl('/course/:id'))
   .add('courseStaff', withBaseUrl('/course/:id/staff'))
