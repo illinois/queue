03dad303e62652b2790f7aa5bde358f834512a6a
Nathan Walters <nwalters512@gmail.com>
2019-01-22 23:19:50 -0600

    Add explanatory comment to axios interceptor

--- src/actions/axios.js
+++ src/actions/axios.js
@@ -5,6 +5,11 @@ import { Router } from '../routes'
 
 const axios = axiosBase.create({ baseURL: baseUrl })
 
+// This is perhaps a bit heavy-handed, but if any of our requests ever come
+// back with a 401, we'll just send the user straight to the login screen.
+// This should only happen if the user's session expires in the middle of
+// them using a page. A possible improvement would be to gracefully prompt
+// them to renew their session, but this will be ok for now.
 axios.interceptors.response.use(null, (err) => {
   if (err.response.status === 401) {
     Router.replaceRoute('login')
