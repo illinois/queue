a6136255bbe630c061b7ec3fc684f8a257986539
Nathan Walters <nwalters512@gmail.com>
2019-01-22 18:20:08 -0600

    Redirect to shib logout in prod

--- src/auth/logout.js
+++ src/auth/logout.js
@@ -1,6 +1,12 @@
-const { withBaseUrl } = require('../util')
+const { withBaseUrl, isDev } = require('../util')
 
 module.exports = (req, res) => {
   res.clearCookie('jwt')
-  res.redirect(withBaseUrl('/'))
+  // When we're running in prod, do another redirect to Shib to complete the
+  // signout process. Locally, just send them back to the login page.
+  if (isDev) {
+    res.redirect(withBaseUrl('/login'))
+  } else {
+    res.redirect('/Shibboleth.sso/Logout')
+  }
 }
