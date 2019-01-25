74ba1d5aa1ba53b60ba1b52bd637e0c5a46b4776
James Wang <jameswang9909@hotmail.com>
2019-01-25 16:27:18 -0600

    Bolden queue name

--- src/components/ConfirmDeleteQueueModal.js
+++ src/components/ConfirmDeleteQueueModal.js
@@ -5,14 +5,19 @@ import ConfirmModal from './ConfirmModal'
 const ConfirmDeleteQueueModal = props => {
   const fullName =
     props.queueName && props.courseName
-      ? `${props.courseName} ${props.queueName}`
-      : 'This queue'
+      ? `${props.courseName} - ${props.queueName}`
+      : ''
   return (
     <ConfirmModal
       isOpen={props.isOpen}
       toggle={props.toggle}
       confirm={props.confirm}
-      descText={`${fullName} and all its open questions will be deleted. This cannot be undone.`}
+      descText={
+        <p>
+          The queue <b>{fullName}</b> and all its open questions will be
+          deleted. This cannot be undone.
+        </p>
+      }
       confirmText="Delete queue"
     />
   )
