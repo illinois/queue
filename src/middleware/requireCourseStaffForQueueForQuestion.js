b2046d0c2957957a7abd12027393725c10e25ad3
James Wang <jameswang9909@hotmail.com>
2019-01-28 18:43:35 -0600

    Add messages for non 2xx responses

--- src/middleware/requireCourseStaffForQueueForQuestion.js
+++ src/middleware/requireCourseStaffForQueueForQuestion.js
@@ -2,17 +2,17 @@ const { Course, Queue, Question } = require('../models')
 
 module.exports = async (req, res, next) => {
   if (!req.params.questionId) {
-    res.status(500).send()
+    res.status(500).send('Invalid question ID')
   } else {
     const questionId = Number.parseInt(req.params.questionId, 10)
     if (Number.isNaN(questionId)) {
-      res.status(500).send()
+      res.status(500).send('Invalid question ID')
       return
     }
 
     const question = await Question.findById(questionId)
     if (!question) {
-      res.status(404).send()
+      res.status(404).send('Question does not exist')
       return
     }
     const { queueId } = question
@@ -30,14 +30,14 @@ module.exports = async (req, res, next) => {
     })
 
     if (!course) {
-      res.status(404).send()
+      res.status(404).send('Course does not exist')
     } else if (res.locals.userAuthz.isAdmin) {
       // Admins can do anything course staff can!
       next()
     } else if (
       res.locals.userAuthz.staffedCourseIds.indexOf(course.id) === -1
     ) {
-      res.status(403).send()
+      res.status(403).send("You don't have authorization to do this")
     } else {
       next()
     }
