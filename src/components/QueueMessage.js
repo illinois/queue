3bb58a1850544408b6e25feae0be356eb6de2e27
Nathan Walters <nwalters512@gmail.com>
2019-02-14 19:43:52 -0600

    Rewrite QueueMessage with hooks

--- src/components/QueueMessage.js
+++ src/components/QueueMessage.js
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import PropTypes from 'prop-types'
 import classnames from 'classnames'
+import { useInput } from 'react-hanger'
 
 import {
   Alert,
@@ -19,189 +20,154 @@ import {
 import ReactMarkdown from 'react-markdown'
 import ParrotText from './ParrotText'
 
-class QueueMessage extends React.Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      editing: false,
-      editedMessage: '',
-      activeTab: '1',
+const QueueMessage = props => {
+  const [editing, setEditing] = useState(false)
+  const editedMessage = useInput('')
+  const [activeTab, setActiveTab] = useState('1')
+  const inputRef = useRef(null)
+  useEffect(() => {
+    if (editing && inputRef.current) {
+      inputRef.current.focus()
     }
-
-    this.onMessageChanged = this.onMessageChanged.bind(this)
-    this.onStartEdit = this.onStartEdit.bind(this)
-    this.onCancelEdit = this.onCancelEdit.bind(this)
-    this.onFinishEdit = this.onFinishEdit.bind(this)
-    this.onChangeTab = this.onChangeTab.bind(this)
-
-    this.inputRef = React.createRef()
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    // If we we just started editing or we switched to the editor tab from
-    // another tab, focus the editor input
-    const { editing, activeTab } = this.state
-    const startedEditing = !prevState.editing && editing
-    const switchedToEditor = prevState.activeTab !== '1' && activeTab === '1'
-    if ((startedEditing || switchedToEditor) && this.inputRef.current) {
-      this.inputRef.current.focus()
+  }, [editing])
+  useEffect(() => {
+    if (activeTab === '1' && inputRef.current) {
+      inputRef.current.focus()
     }
+  }, [activeTab])
+
+  const { message, isUserCourseStaff } = props
+
+  // If the user is not on course staff and the message is null or empty,
+  // don't render the panel to them.
+  if (!isUserCourseStaff && !message) {
+    return null
   }
 
-  onMessageChanged(e) {
-    this.setState({
-      editedMessage: e.target.value,
-    })
+  const handleTabClick = (e, tab) => {
+    e.stopPropagation()
+    e.preventDefault()
+    setActiveTab(tab)
   }
 
-  onStartEdit() {
-    this.setState({
-      editing: true,
-      editedMessage: this.props.message || '',
-    })
-  }
-
-  onCancelEdit() {
-    this.setState({
-      editing: false,
-    })
-  }
-
-  onFinishEdit() {
-    const attributes = {
-      message: this.state.editedMessage,
-    }
-    this.props.updateQueue(this.props.queueId, attributes).then(() => {
-      this.setState({ editing: false })
-    })
-  }
-
-  onChangeTab(tab) {
-    if (this.state.activeTab !== tab) {
-      this.setState({ activeTab: tab })
-    }
-  }
-
-  render() {
-    const { message, isUserCourseStaff } = this.props
-    const { editing, editedMessage } = this.state
-
-    // If the user is not on course staff and the message is null or empty,
-    // don't render the panel to them.
-    if (!isUserCourseStaff && !message) {
-      return null
-    }
-
-    const handleTabClick = (e, tab) => {
+  const handleTabKeyPress = (e, tab) => {
+    if (e.key === 'Enter' || e.key === ' ') {
       e.stopPropagation()
-      e.preventDefault()
-      this.onChangeTab(tab)
+      setActiveTab(tab)
     }
+  }
 
-    const handleTabKeyPress = (e, tab) => {
-      if (e.key === 'Enter' || e.key === ' ') {
-        e.stopPropagation()
-        this.onChangeTab(tab)
-      }
+  const startEditing = () => {
+    setEditing(true)
+    editedMessage.setValue(props.message || '')
+  }
+
+  const finishEditing = () => {
+    const attributes = {
+      message: editedMessage.value,
     }
+    props.updateQueue(props.queueId, attributes).then(() => {
+      setEditing(false)
+    })
+  }
 
-    // We use a custom renderer for text so that we can support parrots!
-    const renderers = {
-      text: props => <ParrotText text={props.children} />,
-    }
+  // We use a custom renderer for text so that we can support parrots!
+  const renderers = {
+    text: element => <ParrotText text={element.children} />,
+  }
 
-    let content
-    let button
-    if (editing) {
-      content = (
-        <Card>
-          <CardHeader>
-            <Nav card tabs>
-              <NavItem>
-                <NavLink
-                  href="#"
-                  className={classnames({
-                    active: this.state.activeTab === '1',
-                  })}
-                  onClick={e => handleTabClick(e, '1')}
-                  onKeyPress={e => handleTabKeyPress(e, '1')}
-                  role="tab"
-                  aria-selected={this.state.activeTab === '1'}
-                >
-                  Edit
-                </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink
-                  href="#"
-                  className={classnames({
-                    active: this.state.activeTab === '2',
-                  })}
-                  onClick={e => handleTabClick(e, '2')}
-                  onKeyPress={e => handleTabKeyPress(e, '2')}
-                  role="tab"
-                  aria-selected={this.state.activeTab === '2'}
-                >
-                  Preview
-                </NavLink>
-              </NavItem>
-            </Nav>
-          </CardHeader>
-          <CardBody className="p-0">
-            <TabContent activeTab={this.state.activeTab}>
-              <TabPane className="m-2" tabId="1">
-                <Input
-                  type="textarea"
-                  name="text"
-                  rows="6"
-                  value={editedMessage}
-                  onChange={this.onMessageChanged}
-                  innerRef={this.inputRef}
-                />
-                <FormText color="muted">
-                  You can use Markdown to format this message.
-                </FormText>
-              </TabPane>
-              <TabPane className="m-3" tabId="2">
-                <ReactMarkdown
-                  source={
-                    editedMessage === '' ? 'Nothing to preview' : editedMessage
-                  }
-                  renderers={renderers}
-                />
-              </TabPane>
-            </TabContent>
-          </CardBody>
-        </Card>
-      )
-      button = (
-        <>
-          <Button color="primary" onClick={this.onFinishEdit}>
-            Save
-          </Button>
-          <Button color="danger" className="ml-1" onClick={this.onCancelEdit}>
-            Cancel
-          </Button>
-        </>
-      )
-    } else {
-      content = <ReactMarkdown source={message} renderers={renderers} />
-      button = (
-        <Button color="primary" onClick={this.onStartEdit}>
-          Edit
+  let content
+  let button
+  if (editing) {
+    content = (
+      <Card>
+        <CardHeader>
+          <Nav card tabs>
+            <NavItem>
+              <NavLink
+                href="#"
+                className={classnames({
+                  active: activeTab === '1',
+                })}
+                onClick={e => handleTabClick(e, '1')}
+                onKeyPress={e => handleTabKeyPress(e, '1')}
+                role="tab"
+                aria-selected={activeTab === '1'}
+              >
+                Edit
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink
+                href="#"
+                className={classnames({
+                  active: activeTab === '2',
+                })}
+                onClick={e => handleTabClick(e, '2')}
+                onKeyPress={e => handleTabKeyPress(e, '2')}
+                role="tab"
+                aria-selected={activeTab === '2'}
+              >
+                Preview
+              </NavLink>
+            </NavItem>
+          </Nav>
+        </CardHeader>
+        <CardBody className="p-0">
+          <TabContent activeTab={activeTab}>
+            <TabPane className="m-2" tabId="1">
+              <Input
+                type="textarea"
+                name="text"
+                rows="6"
+                value={editedMessage.value}
+                onChange={editedMessage.onChanged}
+                innerRef={inputRef}
+              />
+              <FormText color="muted">
+                You can use Markdown to format this message.
+              </FormText>
+            </TabPane>
+            <TabPane className="m-3" tabId="2">
+              <ReactMarkdown
+                source={editedMessage.value || 'Nothing to preview'}
+                renderers={renderers}
+              />
+            </TabPane>
+          </TabContent>
+        </CardBody>
+      </Card>
+    )
+    button = (
+      <>
+        <Button color="primary" onClick={finishEditing}>
+          Save
         </Button>
-      )
-    }
-
-    return (
-      <Alert color="primary" fade={false}>
-        <h6 className="alert-heading">A message from the queue staff</h6>
-        {content}
-        {isUserCourseStaff && <div className="mt-3">{button}</div>}
-      </Alert>
+        <Button
+          color="danger"
+          className="ml-1"
+          onClick={() => setEditing(false)}
+        >
+          Cancel
+        </Button>
+      </>
+    )
+  } else {
+    content = <ReactMarkdown source={message} renderers={renderers} />
+    button = (
+      <Button color="primary" onClick={startEditing}>
+        Edit
+      </Button>
     )
   }
+
+  return (
+    <Alert color="primary" fade={false}>
+      <h6 className="alert-heading">A message from the queue staff</h6>
+      {content}
+      {isUserCourseStaff && <div className="mt-3">{button}</div>}
+    </Alert>
+  )
 }
 
 QueueMessage.defaultProps = {
