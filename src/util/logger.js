8ebe31671584de9e2e5457143d361b93a5b10d66
Nathan Walters <nwalters512@gmail.com>
2019-04-02 17:11:18 -0500

    Improve how logger handles errors and strings

--- src/util/logger.js
+++ src/util/logger.js
@@ -1,13 +1,22 @@
 const { createLogger, transports, format } = require('winston')
+const { inspect } = require('util')
 
-const { combine, colorize, timestamp, align, printf } = format
+const { combine, colorize, timestamp, errors, splat } = format
+
+const outputFormat = format.printf(info => {
+  const formattedMessage =
+    typeof info.message === 'string' ? info.message : inspect(info.message)
+  const message = info.stack || formattedMessage
+  return `${info.timestamp} [${info.level}]: ${message}`
+})
 
 module.exports = createLogger({
   format: combine(
+    errors({ stack: true }),
     colorize(),
     timestamp(),
-    align(),
-    printf(info => `${info.timestamp} [${info.level}]: ${info.message}`)
+    splat(),
+    outputFormat
   ),
   transports: [
     new transports.Console({
