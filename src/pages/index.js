bf80e65b6249200de4204c3e3eda807743efe7b2
Nathan Walters <nwalters512@gmail.com>
2019-04-02 10:21:52 -0500

    Add missing component

--- src/pages/index.js
+++ src/pages/index.js
@@ -113,6 +113,7 @@ class Index extends React.Component {
     return (
       <Container>
         <DevWorkshopAd />
+        <StackRebrandingAlert />
         <div className="d-flex flex-wrap align-items-center mb-4">
           <h1 className="display-4 d-inline-block mb-0 mt-3 mr-auto pr-3">
             Open queues
