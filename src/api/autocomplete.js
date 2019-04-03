a06de734ad44a5544ed0b8abd7fb4cce64519e97
Nathan Walters <nwalters512@gmail.com>
2019-04-03 15:36:39 -0500

    Work on autocomplete api

---
+++ src/api/autocomplete.js
@@ -0,0 +1,28 @@
+const router = require('express').Router()
+
+const { User, Sequelize } = require('../models')
+
+const { failIfErrors, ApiError } = require('./util')
+const requireAdmin = require('../middleware/requireAdmin')
+const safeAsync = require('../middleware/safeAsync')
+
+router.get(
+  '/users',
+  [requireAdmin, failIfErrors],
+  safeAsync(async (req, res, next) => {
+    const { q } = req.query
+    if (!q) {
+      next(new ApiError(422, 'No query specified'))
+      return
+    }
+    const users = await User.findAll({
+      where: {
+        [Sequelize.Op.like]: `%${q}%`,
+      },
+      limit: 10,
+    })
+    res.send(users)
+  })
+)
+
+module.exports = router
