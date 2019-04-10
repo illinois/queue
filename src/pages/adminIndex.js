179e7302b0b62294ceefa86c291710fecb0217c6
Nathan Walters <nwalters512@gmail.com>
2019-04-10 12:39:08 -0500

    Fix linter errors

--- src/pages/adminIndex.js
+++ src/pages/adminIndex.js
@@ -1,19 +1,22 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import { Container } from 'reactstrap'
 
 import PageWithUser from '../components/PageWithUser'
 import AdminUsersPanel from '../components/admin/AdminUsersPanel'
 
-// eslint-disable-next-line react/prefer-stateless-function
-class AdminIndex extends React.Component {
-  render() {
-    return (
-      <Container>
-        <h1 className="display-4">Admin</h1>
-        <AdminUsersPanel user={this.props.user} />
-      </Container>
-    )
-  }
+const AdminIndex = ({ user }) => (
+  <Container>
+    <h1 className="display-4">Admin</h1>
+    <AdminUsersPanel user={user} />
+  </Container>
+)
+
+AdminIndex.propTypes = {
+  user: PropTypes.shape({
+    name: PropTypes.string,
+    netid: PropTypes.string,
+  }).isRequired,
 }
 
-export default PageWithUser(AdminIndex)
+export default PageWithUser(AdminIndex, { requireAdmin: true })
