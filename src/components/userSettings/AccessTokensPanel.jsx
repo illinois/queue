8170ea1800fdbee283a83c45ce3cdc8024cbab17
Nathan Walters <nwalters512@gmail.com>
2019-04-14 19:54:31 -0500

    Add loading and empty states to token list

--- src/components/userSettings/AccessTokensPanel.jsx
+++ src/components/userSettings/AccessTokensPanel.jsx
@@ -10,12 +10,14 @@ import {
   InputGroupAddon,
   Button,
   ListGroup,
+  ListGroupItem,
 } from 'reactstrap'
 import { useInput } from 'react-hanger'
 
 import axios from '../../actions/axios'
 
 import AccessTokenListGroupItem from './AccessTokenListGroupItem'
+import Loading from '../Loading'
 
 const AccessTokensPanel = () => {
   const newTokenNameInput = useInput('')
@@ -54,6 +56,31 @@ const AccessTokensPanel = () => {
       .catch(err => console.error(err))
   }
 
+  let listContent
+  if (tokensLoading) {
+    listContent = (
+      <ListGroupItem key="__loading">
+        <Loading />
+      </ListGroupItem>
+    )
+  } else if (tokens.length === 0) {
+    listContent = (
+      <ListGroupItem key="__none">
+        <span className="text-muted">
+          You don&apos;t have any access tokens right now.
+        </span>
+      </ListGroupItem>
+    )
+  } else {
+    listContent = tokens.map(token => (
+      <AccessTokenListGroupItem
+        {...token}
+        key={token.id}
+        onDeleteToken={() => deleteToken(token.id)}
+      />
+    ))
+  }
+
   return (
     <Card className="mb-3">
       <CardHeader>
@@ -65,15 +92,7 @@ const AccessTokensPanel = () => {
         You can create access tokens to access the Queue API without using
         Shibboleth authentication.
       </CardBody>
-      <ListGroup flush>
-        {tokens.map(token => (
-          <AccessTokenListGroupItem
-            {...token}
-            key={token.id}
-            onDeleteToken={() => deleteToken(token.id)}
-          />
-        ))}
-      </ListGroup>
+      <ListGroup flush>{listContent}</ListGroup>
       <CardBody className="bg-light">
         <Form autoComplete="off" onSubmit={createToken}>
           <InputGroup>
