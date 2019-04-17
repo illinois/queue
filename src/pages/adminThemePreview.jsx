6bc29568378eca021970bc9358075733121bc860
Nathan Walters <nwalters512@gmail.com>
2019-04-17 02:13:51 -0500

    Preview modals and outline buttons

--- src/pages/adminThemePreview.jsx
+++ src/pages/adminThemePreview.jsx
@@ -1,5 +1,14 @@
 import React from 'react'
-import { Container, Alert, Badge, Button } from 'reactstrap'
+import {
+  Container,
+  Alert,
+  Badge,
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+} from 'reactstrap'
 import PageWithUser from '../components/PageWithUser'
 
 const colors = [
@@ -13,28 +22,59 @@ const colors = [
   'light',
 ]
 
+// eslint-disable-next-line react/prop-types
+const Header = ({ children }) => <h1 className="mt-4">{children}</h1>
+
 const AdminThemePreview = () => {
   return (
     <Container>
-      <h1>Alerts</h1>
+      <Header>Alerts</Header>
       {colors.map(color => (
         <Alert color={color} key={color}>
           Here&apos;s a {color} alert. Neat, huh?
         </Alert>
       ))}
-      <h1>Badges</h1>
+      <Header>Badges</Header>
       {colors.map(color => (
         <Badge color={color} key={color} className="mr-2 mb-2">
           {color}
         </Badge>
       ))}
-      <h1>Buttons</h1>
+      <Header>Buttons</Header>
       {colors.map(color => (
         <Button color={color} className="mr-3 mb-3" key={color}>
           {color}
         </Button>
       ))}
-      <h1>Background utilities</h1>
+      <br />
+      {colors.map(color => (
+        <Button color={color} outline className="mr-3 mb-3" key={color}>
+          {color}
+        </Button>
+      ))}
+      <Header>Modals</Header>
+      <div
+        className="modal"
+        style={{
+          position: 'relative',
+          top: 'auto',
+          right: 'auto',
+          bottom: 'auto',
+          left: 'auto',
+          zIndex: 1,
+          display: 'block',
+        }}
+      >
+        <div className="modal-content">
+          <ModalHeader>Are you sure?</ModalHeader>
+          <ModalBody>Do you really want to do that?</ModalBody>
+          <ModalFooter>
+            <Button color="danger">Yes</Button>
+            <Button color="secondary">No</Button>
+          </ModalFooter>
+        </div>
+      </div>
+      <Header>Background utilities</Header>
       {colors.map(color => (
         <div className={`p-3 mb-3 bg-${color}`}>bg-{color}</div>
       ))}
