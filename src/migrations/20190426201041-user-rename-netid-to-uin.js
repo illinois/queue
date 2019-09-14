91aca527b766ef7d15ec1c98ae5f037025abbac9
James Wang <jameswang9909@hotmail.com>
2019-09-14 13:03:25 -0500

    Append @illinois.edu to the end of all uids

--- src/migrations/20190426201041-user-rename-netid-to-uin.js
+++ src/migrations/20190426201041-user-rename-netid-to-uin.js
@@ -1,9 +1,25 @@
 module.exports = {
   up: async (queryInterface, _Sequelize) => {
     await queryInterface.renameColumn('users', 'netid', 'uid')
+
+    const { sequelize } = queryInterface
+    const users = await queryInterface.sequelize.query(
+      'SELECT uid FROM users',
+      { type: sequelize.QueryTypes.SELECT }
+    )
+    Promise.all(
+      users.map(async user =>
+        sequelize.query('UPDATE users SET uid=? WHERE uid=?', {
+          replacements: [`${user.uid}@illinois.edu`, user.uid],
+        })
+      )
+    )
   },
 
   down: async (queryInterface, _Sequelize) => {
+    await queryInterface.sequelize.query(
+      "UPDATE users SET uid=REPLACE(uid, '@illinois.edu', '')"
+    )
     await queryInterface.renameColumn('users', 'uid', 'netid')
   },
 }
