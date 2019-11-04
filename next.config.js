b6fd6a5b154ea57cda86dbfff04134f55428085c
Nathan Walters <nwalters512@gmail.com>
2019-11-04 09:09:54 +0100

    Add bundle analyzer plugin

--- next.config.js
+++ next.config.js
@@ -1,12 +1,16 @@
 const withCSS = require('@zeit/next-css')
 const withSass = require('@zeit/next-sass')
 const withTypescript = require('@zeit/next-typescript')
+const withBundleAnalyzer = require('@next/bundle-analyzer')
 
-module.exports = withTypescript(
-  withSass(
-    withCSS({
-      useFileSystemPublicRoutes: false,
-      assetPrefix: process.env.BASE_URL || '',
-    })
+// Run `ANALYZE=true npm run build` to generate and view these stats
+module.exports = withBundleAnalyzer(
+  withTypescript(
+    withSass(
+      withCSS({
+        useFileSystemPublicRoutes: false,
+        assetPrefix: process.env.BASE_URL || '',
+      })
+    )
   )
 )
