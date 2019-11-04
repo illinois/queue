d1e60b294afb102f4a3bf7953cbd461dc5b6eaae
Nathan Walters <nwalters512@gmail.com>
2019-11-04 14:52:24 +0100

    Remove moment and react-moment dependencies

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 const withCSS = require('@zeit/next-css')
 const withSass = require('@zeit/next-sass')
 const withTypescript = require('@zeit/next-typescript')
-// const withBundleAnalyzer = require('@next/bundle-analyzer')
-const withBundleAnalyzer = args => args
+// Run `ANALYZE=true npm run build` to generate and view bundle stats
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
 
-// Run `ANALYZE=true npm run build` to generate and view these stats
 module.exports = withBundleAnalyzer(
   withTypescript(
     withSass(
