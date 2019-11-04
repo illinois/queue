b8406618183034a3c67ec608fec6364c511466fb
Nathan Walters <nwalters512@gmail.com>
2019-11-04 14:13:51 +0100

    Remove moment.js from Question component

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 const withCSS = require('@zeit/next-css')
 const withSass = require('@zeit/next-sass')
 const withTypescript = require('@zeit/next-typescript')
-const withBundleAnalyzer = require('@next/bundle-analyzer')
+// const withBundleAnalyzer = require('@next/bundle-analyzer')
+const withBundleAnalyzer = args => args
 
 // Run `ANALYZE=true npm run build` to generate and view these stats
 module.exports = withBundleAnalyzer(
