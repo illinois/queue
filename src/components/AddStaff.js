70e74d18234f76408291497c3c2c422c7a0fdf32
Andrew Stec <andrewstec@gmail.com>
2020-03-24 10:53:34 -0700

    Adding user to props to disable button; adding setUidInput state as prop to bounce back uidInput value

--- src/components/AddStaff.js
+++ src/components/AddStaff.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 import {
   Form,
@@ -13,16 +14,32 @@ import UserAutocomplete from './UserAutocomplete'
 const { uidName, uidArticle } = getConfig().publicRuntimeConfig
 
 const AddStaff = props => {
+  const { user } = props
   const [pendingUser, setPendingUser] = useState([])
+  const [uidInput, setUidInput] = useState()
 
   const handleAddStaff = e => {
     if (e) e.preventDefault()
-    props.onAddStaff(pendingUser[0])
+    console.log(
+      'AddStaff:: handleAddStaff - pendingUser[0] ' +
+        JSON.stringify(pendingUser[0])
+    )
+    console.log(
+      'AddStaff:: handleAddStaff - uidInput ' + JSON.stringify(uidInput)
+    )
+    if (pendingUser[0]) {
+      props.onAddStaff(pendingUser[0].id, null)
+    }
+    console.log(
+      'pending user, which leads to onAddStaff attribute on AddStaff/ element',
+      pendingUser[0]
+    )
+    props.onAddStaff(null, uidInput)
   }
 
   // We want to exclude existing staff from the autocompletion list
   const filterBy = option => {
-    return props.existingStaff.findIndex(user => user === option.id) === -1
+    return props.existingStaff.findIndex(u => u === option.id) === -1
   }
   return (
     <FormGroup>
@@ -35,6 +52,7 @@ const AddStaff = props => {
         <InputGroup>
           <UserAutocomplete
             id="user-input"
+            setUidInput={setUidInput}
             selected={pendingUser}
             onChange={setPendingUser}
             placeholder={`Enter ${uidArticle} ${uidName}`}
@@ -45,7 +63,7 @@ const AddStaff = props => {
               color="primary"
               type="button"
               onClick={handleAddStaff}
-              disabled={pendingUser.length === 0}
+              disabled={user.isAdmin && pendingUser.length === 0}
             >
               Add staff
             </Button>
@@ -57,6 +75,9 @@ const AddStaff = props => {
 }
 
 AddStaff.propTypes = {
+  user: PropTypes.shape({
+    isAdmin: PropTypes.bool,
+  }).isRequired,
   // This is just an array of user IDs (not UIDs)
   existingStaff: PropTypes.arrayOf(PropTypes.number),
   onAddStaff: PropTypes.func.isRequired,
@@ -66,4 +87,8 @@ AddStaff.defaultProps = {
   existingStaff: [],
 }
 
-export default AddStaff
+const mapStateToProps = state => ({
+  user: state.user.user,
+})
+
+export default connect(mapStateToProps)(AddStaff)
